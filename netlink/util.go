378c22a595e7a864a61e0fbac217e96e85212609
Takashi Watanabe <44848317+takaswat@users.noreply.github.com>
2021-03-04 13:45:41 -0800

    retrieve messages until NLMSG_DONE comes (#13)
    * retrieve messages until NLMSG_DONE comes
    
    * print err for debug
    
    * use a context with timeout

--- netlink/util.go
+++ netlink/util.go
@@ -1,11 +1,13 @@
 package netlink
 
 import (
+	"context"
+	"fmt"
 	"syscall"
 )
 
 const (
-	bufferSize = 4096
+	bufferSize = 32768
 )
 
 type netlinkInterface struct {
@@ -26,7 +28,7 @@ func newNetlinkInterface() (*netlinkInterface, error) {
 	return &nli, nil
 }
 
-func (nli *netlinkInterface) post(req []byte) ([]syscall.NetlinkMessage, error) {
+func (nli *netlinkInterface) post(req []byte) error {
 	sa := syscall.SockaddrNetlink{
 		Family: syscall.AF_NETLINK,
 		Pad:    0,
@@ -34,39 +36,59 @@ func (nli *netlinkInterface) post(req []byte) ([]syscall.NetlinkMessage, error) 
 		Groups: 0,
 	}
 
-	err := syscall.Sendto(nli.fd, req, 0, &sa)
-	if err != nil {
-		log.Debugf("failed: %s", err)
-		return nil, err
-	}
+	return syscall.Sendto(nli.fd, req, 0, &sa)
+}
 
-	data := []byte{}
-	buf := [bufferSize]byte{}
-	for {
-		n, err := syscall.Read(nli.fd, buf[:])
-		if err != nil {
-			log.Debugf("failed: %s", err)
-			return nil, err
+func (nli *netlinkInterface) recv(ctx context.Context) ([]byte, error) {
+	bufCh := make(chan []byte)
+	errCh := make(chan error)
+	go func() {
+		ptr := 0
+		buf := make([]byte, bufferSize)
+		for {
+			n, _, err := syscall.Recvfrom(nli.fd, nil, syscall.MSG_PEEK|syscall.MSG_TRUNC)
+			if err != nil {
+				errCh <- fmt.Errorf("scan failed: %w", err)
+				return
+			}
+
+			if n == 0 {
+				errCh <- fmt.Errorf("end of file on the socket")
+				return
+			}
+
+			if n > bufferSize {
+				errCh <- fmt.Errorf("not enough buffer (%d > %d)", n, bufferSize)
+				return
+			}
+
+			log.Debugf("%d bytes in the queue", n)
+
+			n, _, err = syscall.Recvfrom(nli.fd, buf, 0)
+			if err != nil {
+				errCh <- fmt.Errorf("recv failed: %w", err)
+				return
+			}
+
+			log.Debugf("read %d bytes", n)
+			ptr += n
+			if n > 0 {
+				break
+			}
 		}
+		bufCh <- buf[:ptr]
+		return
+	}()
 
-		data = append(data, buf[:n]...)
-		if n != bufferSize {
-			break
-		}
-	}
-
-	if err != nil {
-		log.Debugf("failed: %s", err)
+	select {
+	case buf := <-bufCh:
+		return buf, nil
+	case err := <-errCh:
 		return nil, err
+	case <-ctx.Done():
 	}
 
-	nlms, err := syscall.ParseNetlinkMessage(data)
-	if err != nil {
-		log.Debugf("failed: %s", err)
-		return nil, err
-	}
-
-	return nlms, nil
+	return nil, fmt.Errorf("recv timeout")
 }
 
 func (nli *netlinkInterface) close() error {
