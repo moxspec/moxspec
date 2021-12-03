d6086ac651044803e8e7cb23cd514b810ba294c8
Takashi Watanabe <44848317+takaswat@users.noreply.github.com>
2021-12-03 10:27:35 -0800

    use ipmi raw commands to retrieve information (#25)

--- ipmi/ipmi.go
+++ ipmi/ipmi.go
@@ -1,11 +1,11 @@
 package ipmi
 
 import (
-	"context"
 	"fmt"
+	"net"
 	"os/exec"
+	"strconv"
 	"strings"
-	"time"
 
 	"github.com/moxspec/moxspec/loglet"
 	"github.com/moxspec/moxspec/platform"
@@ -20,13 +20,25 @@ func init() {
 
 // Device represents a ipmi device
 type Device struct {
-	Path     string
-	Firmware string
-	MAC      string
-	IPAddr   string
-	Netmask  string
-	Gateway  string
+	Path          string
+	Firmware      string
+	MAC           string
+	IPAddr        string
+	Netmask       string
+	Gateway       string
+	AddressSource addrSrcType
+	VLANID        uint16
 }
+
+type addrSrcType string
+
+const (
+	srcUnspecified addrSrcType = "Unspecified"                   // 0x00
+	srcStatic      addrSrcType = "Static"                        // 0x01
+	srcDHCP        addrSrcType = "DHCP"                          // 0x02
+	srcBIOS        addrSrcType = "BIOS or System software"       // 0x03
+	srcOther       addrSrcType = "Other address assignment pool" // 0x04
+)
 
 // NewDecoder creates and initializes a Device as Decoder
 func NewDecoder() *Device {
@@ -45,50 +57,145 @@ func (d *Device) Decode() error {
 		return fmt.Errorf("ipmi device was not found")
 	}
 
-	cmdpath, err := exec.LookPath("ipmitool")
+	_, err := exec.LookPath("ipmitool")
 	if err != nil {
 		return fmt.Errorf("ipmitool is not installed")
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-	rev, err := util.LoadStringWithContext(ctx, "/sys/class/ipmi/ipmi0/device/bmc/firmware_revision")
-	if err != nil {
-		return err
-	}
-	d.Firmware = rev
-
-	log.Debugf("running %s lan print", cmdpath)
-	res, _ := util.Exec(cmdpath, "lan", "print") // ipmitool always return 1
-	d.IPAddr, d.Netmask, d.Gateway, d.MAC = parseLanPrint(res)
+	d.Firmware = getFirmwareRev()
+	d.MAC = getMACAddress()
+	d.IPAddr = getIPAddress()
+	d.Netmask = getSubnetMask()
+	d.Gateway = getDefaultGateway()
+	d.VLANID = getVLANID()
+	d.AddressSource = getAddressSource()
 
 	return nil
 }
 
-func parseLanPrint(res string) (ipaddr, netmask, gateway, mac string) {
-	for _, l := range strings.Split(res, "\n") {
-		log.Debug(l)
-
-		kv := strings.SplitN(l, ":", 2)
-		if len(kv) != 2 {
-			continue
-		}
-
-		key := strings.TrimSpace(kv[0])
-		val := strings.TrimSpace(kv[1])
-
-		switch key {
-		case "IP Address":
-			ipaddr = val
-		case "Subnet Mask":
-			netmask = val
-		case "Default Gateway IP":
-			gateway = val
-		case "MAC Address":
-			mac = val
-		}
+func getFirmwareRev() string {
+	res, err := util.Exec("ipmitool", "raw", "0x06", "0x01")
+	if err != nil {
+		return ""
 	}
-	return
+	return parseMcInfoRaw(res)
+}
+
+func parseMcInfoRaw(in string) string {
+	codes := strings.Fields(in)
+	if len(codes) < 4 {
+		return ""
+	}
+	fr1, _ := strconv.ParseUint(codes[2], 16, 8)
+	maj := fr1 & 0x7F // binary encoded
+	min := codes[3]   // BCD encoded
+	return fmt.Sprintf("%d.%s", maj, min)
+}
+
+func getMACAddress() string {
+	res, err := util.Exec("ipmitool", "raw", "0x0c", "0x02", "0x01", "0x05", "  0x00", "0x00")
+	if err != nil {
+		return ""
+	}
+	return parseMACAddressRaw(res)
+}
+
+func parseMACAddressRaw(in string) string {
+	codes := strings.Fields(in)
+	if len(codes) != 7 {
+		return ""
+	}
+	return strings.Join(codes[1:], ":")
+}
+
+func getIPAddress() string {
+	res, err := util.Exec("ipmitool", "raw", "0x0c", "0x02", "0x01", "0x03", "  0x00", "0x00")
+	if err != nil {
+		return ""
+	}
+	return parseIPAddressRaw(res)
+}
+
+func getSubnetMask() string {
+	res, err := util.Exec("ipmitool", "raw", "0x0c", "0x02", "0x01", "0x06", "  0x00", "0x00")
+	if err != nil {
+		return ""
+	}
+	return parseIPAddressRaw(res)
+}
+
+func getDefaultGateway() string {
+	res, err := util.Exec("ipmitool", "raw", "0x0c", "0x02", "0x01", "0x0c", "  0x00", "0x00")
+	if err != nil {
+		return ""
+	}
+	return parseIPAddressRaw(res)
+}
+
+func parseIPAddressRaw(in string) string {
+	codes := strings.Fields(in)
+	if len(codes) != 5 { // TBC: IPv6
+		return ""
+	}
+
+	a, _ := strconv.ParseUint(codes[1], 16, 8)
+	b, _ := strconv.ParseUint(codes[2], 16, 8)
+	c, _ := strconv.ParseUint(codes[3], 16, 8)
+	d, _ := strconv.ParseUint(codes[4], 16, 8)
+	return net.IPv4(byte(a), byte(b), byte(c), byte(d)).String()
+}
+
+func getVLANID() uint16 {
+	res, err := util.Exec("ipmitool", "raw", "0x0c", "0x02", "0x01", "0x14", "  0x00", "0x00")
+	if err != nil {
+		return 0
+	}
+	return parseVLANID(res)
+}
+
+func parseVLANID(in string) uint16 {
+	codes := strings.Fields(in)
+	if len(codes) != 3 {
+		return 0
+	}
+	d1, _ := strconv.ParseUint(codes[1], 16, 8)
+	d2, _ := strconv.ParseUint(codes[2], 16, 8)
+
+	if byte(d2)&0x80 != 0x80 { // 1 == enabled, 0 == disabled
+		return 0
+	}
+
+	return ((uint16(d2)&0x0F)<<8 | uint16(d1))
+}
+
+func getAddressSource() addrSrcType {
+	res, err := util.Exec("ipmitool", "raw", "0x0c", "0x02", "0x01", "0x04", "  0x00", "0x00")
+	if err != nil {
+		return "uns[ecified"
+	}
+	return parseAddressSource(res)
+}
+
+func parseAddressSource(in string) addrSrcType {
+	codes := strings.Fields(in)
+	if len(codes) != 2 {
+		return srcUnspecified
+	}
+
+	d1, _ := strconv.ParseUint(codes[1], 16, 8)
+	switch byte(d1) & 0x0F {
+	case 0x00:
+		return srcUnspecified
+	case 0x01:
+		return srcStatic
+	case 0x02:
+		return srcDHCP
+	case 0x03:
+		return srcBIOS
+	case 0x04:
+		return srcOther
+	}
+	return srcUnspecified
 }
 
 // GetSEL returns system event list
--- ipmi/ipmi_test.go
+++ ipmi/ipmi_test.go
@@ -5,47 +5,134 @@ import (
 	"testing"
 )
 
-func TestParseLanPrint(t *testing.T) {
-	var ipaddr, netmask, gateway, mac string
+func TestParseMcInfoRaw(t *testing.T) {
+	var rev string
 	tests := []struct {
-		in      string
-		ipaddr  string
-		netmask string
-		gateway string
-		mac     string
+		in string
+		ex string
 	}{
-		{`Set in Progress         : Set Complete
-IP Address Source       : DHCP Address
-IP Address              : 10.17.7.36
-Subnet Mask             : 255.0.0.0
-MAC Address             : 5c:54:6d:0b:54:6b
-SNMP Community String   : TrapAdmin12#$
-IP Header               : TTL=0x40 Flags=0x40 Precedence=0x00 TOS=0x10
-Default Gateway IP      : 0.0.0.0
-802.1q VLAN ID          : Disabled
-RMCP+ Cipher Suites     : 0,1,2,3,17
-Cipher Suite Priv Max   : XuuaXXXXXXXXXXX
-                        :     X=Cipher Suite Unused
-                        :     c=CALLBACK
-                        :     u=USER
-                        :     o=OPERATOR
-                        :     a=ADMIN
-                        :     O=OEM`, "10.17.7.36", "255.0.0.0", "0.0.0.0", "5c:54:6d:0b:54:6b"},
-		{`garbage : garbage 
-dummy : dummy 
-mox : mox 
-248 : 248 
-fail : fail`, "", "", "", ""},
+		{" 01 81 18 08 02 af db 07 00 01 00 19 00 10 00", "24.08"},
+		{" 01 81 02 48 02 af db 07 00 01 00 19 00 10 00", "2.48"},
+		{" 20 01 01 17 02 bf af 2b 00 02 02 12 00 17 00", "1.17"},
+		{" 01 81 00 00 02 af db 07 00 01 00 19 00 10 00", "0.00"},
+		{" 20 81 0c 03 02 bf af 2b 00 05 00 00 00 00 00", "12.03"},
+		{" 20 81 ff 03 02 bf af 2b 00 05 00 00 00 00 00", "127.03"},
+		{" 01 81 02 92 02 af db 07 00 01 00 19 00 10 00", "2.92"},
+		{" 01 81 02", ""},
+		{" 01 81 02 48", "2.48"},
+		{"248", ""},
 	}
 
 	for _, test := range tests {
 		tt := test
 
 		t.Run(fmt.Sprintf("%+v", tt), func(t *testing.T) {
-			ipaddr, netmask, gateway, mac = parseLanPrint(tt.in)
-			if ipaddr != tt.ipaddr || netmask != tt.netmask || gateway != tt.gateway || mac != tt.mac {
-				fmtr := "got: ipaddr %s / netmask %s / gw: %s / mac %s, expect: ipaddr %s / netmask %s / gw %s / mac %s"
-				t.Errorf(fmtr, ipaddr, netmask, gateway, mac, tt.ipaddr, tt.netmask, tt.gateway, tt.mac)
+			rev = parseMcInfoRaw(tt.in)
+			if rev != tt.ex {
+				t.Errorf("got: %s, expected: %s", rev, tt.ex)
+			}
+		})
+	}
+}
+
+func TestParseMacAddressRaw(t *testing.T) {
+	var rev string
+	tests := []struct {
+		in string
+		ex string
+	}{
+		{" 11 5c 54 6d 0b 54 6b", "5c:54:6d:0b:54:6b"},
+		{" 11 0c 42 a1 4d 91 c4", "0c:42:a1:4d:91:c4"},
+		{" 11 5c 54 6d 0b 54 6b 24", ""},
+		{" 24 82 48 ", ""},
+		{"248", ""},
+		{"", ""},
+	}
+
+	for _, test := range tests {
+		tt := test
+
+		t.Run(fmt.Sprintf("%+v", tt), func(t *testing.T) {
+			rev = parseMACAddressRaw(tt.in)
+			if rev != tt.ex {
+				t.Errorf("got: %s, expected: %s", rev, tt.ex)
+			}
+		})
+	}
+}
+
+func TestParseIPAddressRaw(t *testing.T) {
+	var rev string
+	tests := []struct {
+		in string
+		ex string
+	}{
+		{" 11 00 00 00 00", "0.0.0.0"},
+		{" 11 ff 00 00 00", "255.0.0.0"},
+		{" 11 0a 17 fd 8b", "10.23.253.139"},
+		{" 24 82 48 ", ""},
+		{"248", ""},
+		{"", ""},
+	}
+
+	for _, test := range tests {
+		tt := test
+
+		t.Run(fmt.Sprintf("%+v", tt), func(t *testing.T) {
+			rev = parseIPAddressRaw(tt.in)
+			if rev != tt.ex {
+				t.Errorf("got: %s, expected: %s", rev, tt.ex)
+			}
+		})
+	}
+}
+
+func TestParseVLANID(t *testing.T) {
+	var rev uint16
+	tests := []struct {
+		in string
+		ex uint16
+	}{
+		{" 11", 0},
+		{" 11 c8 80", 200},
+	}
+
+	for _, test := range tests {
+		tt := test
+
+		t.Run(fmt.Sprintf("%+v", tt), func(t *testing.T) {
+			rev = parseVLANID(tt.in)
+			if rev != tt.ex {
+				t.Errorf("got: %d, expected: %d", rev, tt.ex)
+			}
+		})
+	}
+}
+
+func TestParseAddrSrc(t *testing.T) {
+	var rev addrSrcType
+	tests := []struct {
+		in string
+		ex addrSrcType
+	}{
+		{"", srcUnspecified},
+		{" 11", srcUnspecified},
+		{" 11 00", srcUnspecified},
+		{" 11 00 00", srcUnspecified},
+		{" 11 01", srcStatic},
+		{" 11 02", srcDHCP},
+		{" 11 03", srcBIOS},
+		{" 11 04", srcOther},
+		{" 11 05", srcUnspecified},
+	}
+
+	for _, test := range tests {
+		tt := test
+
+		t.Run(fmt.Sprintf("%+v", tt), func(t *testing.T) {
+			rev = parseAddressSource(tt.in)
+			if rev != tt.ex {
+				t.Errorf("got: %s, expected: %s", rev, tt.ex)
 			}
 		})
 	}
